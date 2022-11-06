ac568f995b10d31646f74eb7aa9c9a402fc0c36f
jeffersondlopes <jeffersondlopes@gmail.com>
2022-11-06 13:05:10 -0300

    Ajust pom mapstruct/lombok

--- src/main/java/br/com/person/utils/mapper/PersonMapper.java
+++ src/main/java/br/com/person/utils/mapper/PersonMapper.java
@@ -3,11 +3,12 @@ package br.com.person.utils.mapper;
 import br.com.person.dto.request.PersonRequest;
 import br.com.person.dto.response.PersonResponse;
 import br.com.person.entities.Person;
+import br.com.person.entities.TypePerson;
 import org.mapstruct.InjectionStrategy;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR, uses = {TypePersonEnum.class})
+@Mapper(componentModel = "spring", injectionStrategy = InjectionStrategy.CONSTRUCTOR)
 public interface PersonMapper {
 
     @Mapping(target = "person", ignore = true)
@@ -15,7 +16,7 @@ public interface PersonMapper {
     @Mapping(source = "name", target = "person.name")
     @Mapping(source = "shortName", target = "person.shortName")
     @Mapping(source = "documentNumber", target = "person.documentNumber")
-    //@Mapping(source = "typePersonId", target = "person.typePersonId")
+    @Mapping(source = "typePersonId", target = "person.typePerson")
     @Mapping(source = "EMail", target = "person.eMail")
     Person toPerson(PersonRequest request);
 
@@ -23,8 +24,16 @@ public interface PersonMapper {
     @Mapping(source = "person.name", target = "name")
     @Mapping(source = "person.shortName", target = "shortName")
     @Mapping(source = "person.documentNumber", target = "documentNumber")
-    //@Mapping(source = "person.typePersonId", target = "typePersonId")
+    @Mapping(source = "person.typePerson", target = "typePersonId")
     @Mapping(source = "person.EMail", target = "eMail")
     PersonResponse toPersonResponde(Person person);
+
+    default TypePerson toTypePersonEnum(Integer typePersonId){
+        return TypePerson.getTypePersonById(typePersonId);
+    }
+
+    default Integer toTypePersonEnum(TypePerson typePerson){
+        return typePerson.getId();
+    }
 
 }
